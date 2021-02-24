66556d661839a689f250b7e541f347b472caaab1
Konrad Nowakowski <konrad.x92@gmail.com>
2021-02-24 13:45:11 +0000

    Fix spikes checking player instance

--- src/aliens/spikes/spikes.gd
+++ src/aliens/spikes/spikes.gd
@@ -4,5 +4,5 @@ func _ready():
 	connect("body_entered", self, "_on_body_entered")
 
 func _on_body_entered(body: PhysicsBody2D):
-	if body is preload("res://player/player.gd"):
+	if body and body.has_method("kill"):
 		body.kill()
