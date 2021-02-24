28379c8a08680d41477f2db25ae86e54abfaf126
Konrad Nowakowski <konrad.x92@gmail.com>
2021-02-24 12:45:19 +0000

    Add skip intro feature

--- src/levels/intro/intro.gd
+++ src/levels/intro/intro.gd
@@ -1,5 +1,7 @@
 extends "res://levels/level_base/level_base.gd"
 
+signal intro_finished()
+
 func _enter_tree():
 	GameUI.hide_ui()
 
@@ -10,7 +12,15 @@ func _ready():
 	$TileMap/Player.freeze()
 	
 	yield(Transition, "fade_in_ended")
+	$AnimationPlayer.connect("animation_finished", self, "_finish_intro")
 	$AnimationPlayer.play("intro")
 	
-	yield($AnimationPlayer, "animation_finished")
+	yield(self, "intro_finished")
 	_on_exit_entered($TileMap/Player)
+
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		_finish_intro()
+
+func _finish_intro():
+	emit_signal("intro_finished")
